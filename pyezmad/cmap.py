6b8bc120c2f800168f4529fd52fd8f606dac8613
Masato Onodera <masato.onodera@gmail.com>
2015-10-25 23:03:00 +0100

    minor change (trying...)

--- pyezmad/cmap.py
+++ pyezmad/cmap.py
@@ -6,18 +6,18 @@ import seaborn.apionly as sns
 
 _cmap_ebv = sns.cubehelix_palette(light=1, as_cmap=True)
 
-# cmap_dict = {'white': cm.Greys_r,
-#              'npix': cm.Greys_r,
-#              'oh12': cm.YlGn,
-#              'ebv': cmap_ebv,
-#              'sfr': cm.YlOrBr,
-#              'vel': cm.Spectral,
-#              'sigma': cm.YlOrRd,
-#              'age_ha': cm.Oranges,
-#              'ew': cm.YlGn_r,
-#              'mass': cm.Purples,
-#              'age_star': cm.Oranges,
-#              'metal_star': cm.PuBu}
+cmap_dict = {'white': cm.Greys_r,
+             'npix': cm.Greys_r,
+             'oh12': cm.YlGn,
+             'ebv': _cmap_ebv,
+             'sfr': cm.YlOrBr,
+             'vel': cm.Spectral,
+             'sigma': cm.YlOrRd,
+             'age_ha': cm.Oranges,
+             'ew': cm.YlGn_r,
+             'mass': cm.Purples,
+             'age_star': cm.Oranges,
+             'metal_star': cm.PuBu}
 
 white = cm.Greys_r
 npix = cm.Greys_r
