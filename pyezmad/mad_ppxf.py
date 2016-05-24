ec7b76a3f2ea250edeaf872214fe067b5b5aa6f9
Masato Onodera <monodera@naoj.org>
2016-05-23 18:12:03 -1000

    - Modified the initial guess to include h3 and h4 to adopt for a recent release. Then, as Michele implement to keep the backward compatibility, I changed the code again back to the original.

--- pyezmad/mad_ppxf.py
+++ pyezmad/mad_ppxf.py
@@ -598,7 +598,7 @@ def run_voronoi_stacked_spectra_all(infile, npy_prefix, npy_dir='.',
             t_begin_each = time.time()
             try:
                 pp = ppxf(stars_templates, galaxy, noise, velscale,
-                          start=[vel_init, sigma_init, 0., 0.],
+                          start=[vel_init, sigma_init],
                           lam=np.exp(logLam_galaxy),
                           goodpixels=goodPixels, vsyst=dv,
                           **ppxf_keydic)
